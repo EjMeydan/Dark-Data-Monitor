f345dbd9fc49661735cbd4422a9248962e511939
Emre Meydan <ej.meydan@outlook.com>
2025-08-16 00:19:47 +0100

    Refactor scanner: return structured file data

--- main.py
+++ main.py
@@ -2,20 +2,45 @@ from pathlib import Path
 from datetime import datetime
 
 def scan_directory(folder_path):
+    """
+    Recursively scan a folder and prints information about each file.
+
+    Parameters: 
+    folder_path (str): The path to the folder to scan
+
+    Returns:
+        list of dict: Each dictionary constains:
+            - 'path': str, full file path
+            - 'size_mb': float, file size in megabytes
+            - 'last_modified': datetime, last modified timestamp
+    """
+
     folder = Path(folder_path)
 
     if not folder.exists():
         print(f"Path {folder_path} does not exist.")
-        return
+        return []
     
-    for file in folder.rglob("*"):
+    files_data = []
+    
+    for file in folder.rglob("*"): # Recursively list all files
         if file.is_file():
+            
             stats = file.stat()
-            size_mb = stats.st_size / (1024 * 1024)
-            last_modified = datetime.fromtimestamp(stats.st_mtime)
+            size_mb = stats.st_size / (1024 * 1024) # Convert bytes to MB
+            last_modified = datetime.fromtimestamp(stats.st_mtime) 
 
-            print(f"{file} | {size_mb:.2f} MB | Last modified: {last_modified}")
+            # Store file info in a dictionary
+            files_data.append({
+                "path": str(file),
+                "size_mb": size_mb,
+                "last_modified": last_modified
+            })
+        
+    return files_data 
 
 
 if __name__ == "__main__":
-    scan_directory(".")
+    files = scan_directory(".")
+    for f in files:
+        print(f)
